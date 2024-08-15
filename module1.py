8eb371e430cf43ea5bb1c1d59c38b7f025833ebf
Sean James <seanjames0114@gmail.com>
2024-08-15 16:27:36 -0600

    completed part 1 and 2

---
+++ module1.py
@@ -0,0 +1,33 @@
+'''
+Write a Python program to find the addition and subtraction of two numbers.
+Ask the user to input two numbers (num1 and num2).
+ Given those two numbers, add them together to find the output.
+ Also, subtract the two numbers to find the output.
+'''
+
+#num 1
+num1 = int(input('Give Number 1 : '))
+
+#num 2 
+num2 = int(input('Give Number 2 : '))
+
+
+print('Part 1')
+
+#Add
+res = num1 + num2
+print('The sum is : ', res)
+
+#Subtract
+res = num1 - num2
+print('The subtraction is : ', res)
+
+print('Part 2')
+
+#multiply
+res = num1 * num2
+print('The Multiplication Result is : ', res)
+
+#Divide
+res = num1/num2
+print('The Division Result is : ', res)
