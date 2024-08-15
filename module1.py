2943ca9c1ba1c8de13038775162f0b1b929f81ae
Sean James <seanjames0114@gmail.com>
2024-08-15 16:29:11 -0600

    Removed comments

--- module1.py
+++ module1.py
@@ -1,10 +1,3 @@
-'''
-Write a Python program to find the addition and subtraction of two numbers.
-Ask the user to input two numbers (num1 and num2).
- Given those two numbers, add them together to find the output.
- Also, subtract the two numbers to find the output.
-'''
-
 #num 1
 num1 = int(input('Give Number 1 : '))
 
